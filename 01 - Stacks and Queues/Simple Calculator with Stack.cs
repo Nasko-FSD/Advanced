6b3b339210dc3324f29602a88f558e9fc5bb7129
Nasko-FSD <atanasnw@gmail.com>
2025-04-02 14:55:31 +0300

    Simple Calculator with Stack

---
+++ 01 - Stacks and Queues/Simple Calculator with Stack.cs
@@ -0,0 +1,21 @@
+﻿var input = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+var result = new Stack<string>(input.Reverse());
+
+while (result.Count > 1)
+{
+    var firstNumber = int.Parse(result.Pop());
+    var operation = result.Pop();
+    var secondNumber = int.Parse(result.Pop());
+
+    var tempResult = operation switch 
+    {
+        "+" => (firstNumber + secondNumber),
+        "-" => (firstNumber - secondNumber),
+        _ => 0
+    };
+    result.Push(tempResult.ToString());
+}
+
+Console.WriteLine(result.Pop());
---
+++ 01 - Stacks and Queues/Stack Sum.cs
@@ -0,0 +1,49 @@
+﻿List<int> inputIntegers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToList();
+
+var stack = new Stack<int>(inputIntegers);
+
+string input = "";
+
+while ((input = Console.ReadLine().ToLower()) != "end")
+{
+    string[] tokens = input
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    string command = tokens[0];
+    
+    if (command == "add")
+    {
+        int firstNumber = int.Parse(tokens[1]);
+        stack.Push(firstNumber);
+        int secondNumebr = int.Parse(tokens[2]);
+        stack.Push(secondNumebr);
+    }
+    else
+    {
+        int givenNumber = int.Parse(tokens[1]);
+
+        if (givenNumber > stack.Count)
+        {
+            continue;
+        }
+        else
+        {
+            while (givenNumber > 0 && stack.Count > 0)
+            {
+                stack.Pop();
+                givenNumber--;
+            }
+        }
+    }
+}
+int sum = 0;
+
+foreach (var number in stack)
+{
+    sum += number;
+}
+
+Console.WriteLine($"Sum: {sum}");
