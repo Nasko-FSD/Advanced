4f41e3a3c2d2eea842fd0f65dd4b940d55bb7289
Nasko-FSD <atanasnw@gmail.com>
2025-04-12 18:10:28 +0300

    Hot Potato

---
+++ 01 - Stacks and Queues/Hot Potato.cs
@@ -0,0 +1,28 @@
+﻿List<string> input = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .ToList();
+
+Queue<string> allInput = new Queue<string>(input);
+
+
+int number = int.Parse(Console.ReadLine());
+
+int currentIndex = 1;
+
+while (allInput.Count > 1)
+{
+    var currentName = allInput.Dequeue();
+
+    if (currentIndex == number)
+    {
+        Console.WriteLine($"Removed {currentName}");
+        currentIndex = 0;
+    }
+    else
+    {
+        allInput.Enqueue(currentName);
+    }
+
+    currentIndex++;
+}
+Console.WriteLine($"Last is {allInput.Dequeue()}");
