6d3e9f537d44e1e307b7ca864e69860c8154e8c0
Nasko-FSD <atanasnw@gmail.com>
2025-04-14 22:48:17 +0300

    Hot Potato Second Solve

---
+++ 01 - Stacks and Queues/Hot Potato Second Solve.cs
@@ -0,0 +1,16 @@
+﻿var input = Console.ReadLine()
+    .Split();
+
+int number = int.Parse(Console.ReadLine());
+
+Queue<string> allInput = new Queue<string>(input);
+
+while (allInput.Count > 1)
+{
+    for (int i = 1; i < number; i++)
+    {
+        allInput.Enqueue(allInput.Dequeue());
+    }
+    Console.WriteLine($"Removed {allInput.Dequeue()}");
+}
+Console.WriteLine($"Last is {allInput.Dequeue()}");
