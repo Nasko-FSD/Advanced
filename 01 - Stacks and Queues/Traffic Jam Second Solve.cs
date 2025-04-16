eb72e593de3482f7a6393652ca9e6ff5bdbbc1dc
Nasko-FSD <atanasnw@gmail.com>
2025-04-16 14:28:29 +0300

    Traffic Jam Second Solve

---
+++ 01 - Stacks and Queues/Traffic Jam Second Solve.cs
@@ -0,0 +1,32 @@
+﻿int passingCars = int.Parse(Console.ReadLine());
+
+Queue<string> cars = new Queue<string>();
+
+int totalCarsPassed = 0;
+
+while (true)
+{
+    string command = Console.ReadLine();
+
+    if (command == "green")
+    {
+        for (int i = 0; i < passingCars; i++)
+        {
+            if (cars.Any())
+            {
+                var currentCar = cars.Dequeue();
+                Console.WriteLine($"{currentCar} passed!");
+                totalCarsPassed++;
+            }
+        }
+    }
+    else if (command == "end")
+    {
+        break;
+    }
+    else 
+    {
+        cars.Enqueue(command);
+    }
+}
+Console.WriteLine($"{totalCarsPassed} cars passed the crossroads.");
