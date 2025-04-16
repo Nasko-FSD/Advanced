10d2f4e537121331a30432c5de817b893f53b2c2
Nasko-FSD <atanasnw@gmail.com>
2025-04-16 16:58:47 +0300

    Supermarket

---
+++ 01 - Stacks and Queues/Supermarket.cs
@@ -0,0 +1,23 @@
+﻿int counter = 0;
+
+string names = "";
+
+Queue<string> queue = new Queue<string>();
+
+while ((names = Console.ReadLine()) != "End")
+{
+    if (names == "Paid")
+    {
+        foreach (var name in queue)
+        {
+            Console.WriteLine(name);
+        }
+        queue.Clear();
+        counter = 0;
+        continue;
+    }
+
+    queue.Enqueue(names);
+    counter++;
+}
+Console.WriteLine($"{counter} people remaining.");
