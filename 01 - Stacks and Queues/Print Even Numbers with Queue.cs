443ebb1c3e39240d0367c05d50d0403db77c5145
Nasko-FSD <atanasnw@gmail.com>
2025-04-16 16:32:37 +0300

    Print Even Numbers with Queue

---
+++ 01 - Stacks and Queues/Print Even Numbers with Queue.cs
@@ -0,0 +1,21 @@
+﻿int[] numbers = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+Queue<int> allNumbers = new Queue<int>(numbers);
+
+for (int i = 0; i < allNumbers.Count; i++)
+{
+    if (allNumbers.Peek() % 2 == 0)
+    {
+        allNumbers.Enqueue(allNumbers.Dequeue());
+    }
+    else
+    {
+        allNumbers.Dequeue();
+        i--;
+    }
+}
+
+Console.WriteLine(string.Join(", ", allNumbers));
