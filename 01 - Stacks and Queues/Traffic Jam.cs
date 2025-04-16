25e17f8d60c7b05adf88b2482d1b2f06d999a8d3
Nasko-FSD <atanasnw@gmail.com>
2025-04-16 13:29:38 +0300

    Traffic Jam

---
+++ 01 - Stacks and Queues/Traffic Jam.cs
@@ -0,0 +1,26 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+Queue<string> allInput = new Queue<string>();
+
+var input = Console.ReadLine();
+
+int totalCount = 0;
+
+while (input != "end")
+{
+    if (input.Contains("green") == false)
+    {
+        allInput.Enqueue(input.ToString());
+    }
+    else
+    {
+        for (int i = 1; i <= number && allInput.Count > 0; i++)
+        {
+            Console.WriteLine($"{allInput.Dequeue()} passed!");
+            totalCount++;
+        }
+    }
+
+    input = Console.ReadLine();
+}
+Console.WriteLine($"{totalCount} cars passed the crossroads.");
