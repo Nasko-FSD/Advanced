e330d466eea74e669feceb90449562b4bef87171
Nasko-FSD <atanasnw@gmail.com>
2025-04-17 17:35:30 +0300

    Basic Stack Operations Third Solve

---
+++ 02 - Stacks and Queues - Exercises/Basic Stack Operations Third Solve.cs
@@ -0,0 +1,48 @@
+﻿int[] input = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+int toEnter = input[0];
+
+int toPop = input[1];
+
+int searchFor = input[2];
+
+
+int[] enterNumbers = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+Stack<int> allNumbers = new Stack<int>(enterNumbers);
+
+for (int i = 0; i < toPop && allNumbers.Count > 0; i++)
+{
+    allNumbers.Pop();
+}
+
+if (allNumbers.Contains(searchFor))
+{
+    Console.WriteLine("true");
+}
+
+else if (allNumbers.Count < 1)
+{
+    Console.WriteLine("0");
+}
+
+
+else
+{
+    int smallestNumber = int.MaxValue;
+
+    foreach (var num in allNumbers)
+    {
+        if (num < smallestNumber)
+        {
+            smallestNumber = num;
+        }    
+    }
+    Console.WriteLine(smallestNumber);
+}
