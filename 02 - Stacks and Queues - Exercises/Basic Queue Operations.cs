05bd555c00a81e27e1e938d483b85fcdd0bffb47
Nasko-FSD <atanasnw@gmail.com>
2025-04-22 15:22:30 +0300

    Basic Queue Operations

---
+++ 02 - Stacks and Queues - Exercises/Basic Queue Operations.cs
@@ -0,0 +1,46 @@
+﻿int[] tokens = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+int totalNumbers = tokens[0];
+
+int toRemove = tokens[1];
+
+int searchFor = tokens[2];
+
+int[] toEnter = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+Queue<int> queue = new Queue<int>(toEnter);
+
+int smallesElement = int.MaxValue;
+
+for (int i = 0; i < toRemove && queue.Any(); i++)
+{
+    queue.Dequeue();
+}
+
+if (queue.Contains(searchFor))
+{
+    Console.WriteLine("true");
+}
+
+else if (queue.Any() == false)
+{
+    Console.WriteLine("0");
+}
+
+else
+{
+    foreach (var element in queue)
+    {
+        if (element < smallesElement)
+        {
+            smallesElement = element;
+        }
+    }
+    Console.WriteLine(smallesElement);
+}
