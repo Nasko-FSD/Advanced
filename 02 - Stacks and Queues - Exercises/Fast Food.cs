94f6aba5aa0eb41537a153b095d1d0a9f55fc39c
Nasko-FSD <atanasnw@gmail.com>
2025-04-23 17:04:19 +0300

    Fast Food

---
+++ 02 - Stacks and Queues - Exercises/Fast Food.cs
@@ -0,0 +1,41 @@
+﻿int totalQuantity = int.Parse(Console.ReadLine());
+
+int[] allOrders = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+Queue<int> queue = new Queue<int>(allOrders);
+
+int biggestOrder = int.MinValue;
+
+foreach (var order in allOrders)
+{
+    if (order > biggestOrder)
+    {
+        biggestOrder = order;
+    }
+}
+
+while (queue.Count > 0)
+{
+    if (totalQuantity >= queue.Peek())
+    {
+        totalQuantity -= queue.Peek();
+        queue.Dequeue();
+    }
+    else
+    {
+        break;
+    }
+}
+Console.WriteLine(biggestOrder);
+
+if (queue.Count == 0)
+{
+    Console.WriteLine("Orders complete");
+}
+else
+{
+    Console.Write("Orders left: {0}", string.Join(" ", queue));
+}
