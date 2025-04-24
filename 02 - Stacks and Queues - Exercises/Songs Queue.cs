08d7390f5044abd4dd19d9186b53c91cbb51d1e5
Nasko-FSD <atanasnw@gmail.com>
2025-04-24 21:00:20 +0300

    Songs Queue

---
+++ 02 - Stacks and Queues - Exercises/Songs Queue.cs
@@ -0,0 +1,43 @@
+﻿string[] inputSongs = Console.ReadLine()
+    .Split(", ")
+    .ToArray();
+
+Queue<string> queue = new Queue<string>(inputSongs);
+
+
+while (queue.Any())
+{
+    string[] enteredCommand = Console.ReadLine()
+        .Split(" ")
+        .ToArray();
+
+    string command = enteredCommand[0];
+
+    switch (command)
+    {
+        case "Play":
+            queue.Dequeue();
+            break;
+
+        case "Add":
+            string songName = string.Join(" ", enteredCommand.Skip(1));
+            if (queue.Contains(songName) == false)
+            {
+                queue.Enqueue(songName);
+            }
+            else
+            {
+                Console.WriteLine($"{songName} is already contained!");
+            }
+            break;
+
+        case "Show":
+            {
+
+                Console.WriteLine(string.Join(", ", queue));
+                break;
+            }
+    }
+
+}
+Console.WriteLine("No more songs!");
