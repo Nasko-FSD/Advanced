ef7c03e696285dac6b7e2ee8955aa9d54e7f24b1
Nasko-FSD <atanasnw@gmail.com>
2025-04-24 17:20:05 +0300

    Fasion Boutique

---
+++ 02 - Stacks and Queues - Exercises/Fasion Boutique.cs
@@ -0,0 +1,31 @@
+﻿int[] clothes = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+clothes.Reverse();
+
+Stack<int> stack = new Stack<int>(clothes);
+
+int rackVolume = int.Parse(Console.ReadLine());
+
+int totalRacks = 1;
+int sumClothes = 0;
+
+while (stack.Any())
+{
+    int currentCloth = stack.Peek();
+
+    if (sumClothes + currentCloth <= rackVolume)
+    {
+        sumClothes += stack.Pop();
+    }
+    else
+    {
+        totalRacks++;
+        sumClothes = 0;
+    }
+
+}
+
+Console.WriteLine(totalRacks);
