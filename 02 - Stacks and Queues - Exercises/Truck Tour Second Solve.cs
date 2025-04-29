17ec34b5133b4b742cf2fcfa42cc4775309bd246
Nasko-FSD <atanasnw@gmail.com>
2025-04-29 14:55:41 +0300

    Truck Tour Second Solve

---
+++ 02 - Stacks and Queues - Exercises/Truck Tour Second Solve.cs
@@ -0,0 +1,45 @@
+﻿int totalPumps = int.Parse(Console.ReadLine());
+Queue<int[]> pumps = new Queue<int[]>();
+
+FillQueue(totalPumps, pumps);
+
+int exactIndex = 0;
+
+while (exactIndex < totalPumps)
+{
+    bool isTrue = true;
+    int fuelAmount = 0;
+
+    foreach (var pump in pumps)
+    {
+        fuelAmount += pump[0];
+        fuelAmount -= pump[1];
+
+        if (fuelAmount < 0)
+        {
+            isTrue = false;
+            break;
+        }
+    }
+
+    if (isTrue)
+    {
+        Console.WriteLine(exactIndex);
+        return;
+    }
+
+    exactIndex++;
+    pumps.Enqueue(pumps.Dequeue());
+}
+
+static void FillQueue(int totalPumps, Queue<int[]> pumps)
+{
+    for (int i = 0; i < totalPumps; i++)
+    {
+        int[] input = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+        pumps.Enqueue(input);
+    }
+}
