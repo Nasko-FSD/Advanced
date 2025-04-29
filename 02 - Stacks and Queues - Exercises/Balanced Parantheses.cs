7ec7b1042104485044447d2f1cf7edf92fad9141
Nasko-FSD <atanasnw@gmail.com>
2025-04-29 22:34:52 +0300

    Balanced Parantheses

---
+++ 02 - Stacks and Queues - Exercises/Balanced Parantheses.cs
@@ -0,0 +1,44 @@
+﻿string input = Console.ReadLine();
+
+Stack<char> stackParentheses = new Stack<char>();
+bool isSuccess = true;
+
+for (int i = 0; i < input.Length; i++)
+{
+    char currentChar = input[i];
+
+    if (currentChar == '{' || currentChar == '[' || currentChar == '(')
+    {
+        stackParentheses.Push(currentChar);
+        continue;
+    }
+
+    if (stackParentheses.Count == 0)
+    {
+        isSuccess = false;
+        break;
+    }
+
+    if (currentChar == '}' && stackParentheses.Peek() == '{'
+        || currentChar == ']' && stackParentheses.Peek() == '['
+        || currentChar == ')' && stackParentheses.Peek() == '(')
+    {
+        stackParentheses.Pop();
+    }
+
+    else
+    {
+        isSuccess = false;
+        break;
+    }
+}
+
+if (isSuccess == true)
+{
+    Console.WriteLine("YES");
+}
+
+else
+{
+    Console.WriteLine("NO");
+}
