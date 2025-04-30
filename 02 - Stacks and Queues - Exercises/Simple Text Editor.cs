cbdbcefc61894a100b636c1a6938063ffb93e93d
Nasko-FSD <atanasnw@gmail.com>
2025-04-30 19:48:29 +0300

    Simple Text Editor

---
+++ 02 - Stacks and Queues - Exercises/Simple Text Editor.cs
@@ -0,0 +1,41 @@
+﻿int totalCount = int.Parse(Console.ReadLine());
+
+Stack<string> stack = new Stack<string>();
+
+string text = "";
+
+for (int i = 0; i < totalCount; i++)
+{
+    string[] input = Console.ReadLine()
+        .Split();
+
+    int command = int.Parse(input[0]);
+
+    switch (command)
+    {
+        case 1:
+            text += input[1];
+            stack.Push(text);
+            break;
+        case 2:
+            int charsToErase = int.Parse(input[1]);
+            text = text.Substring(0, text.Length - charsToErase);
+            stack.Push(text);
+            break;
+        case 3:
+            int index = int.Parse(input[1]);
+            Console.WriteLine(text[index - 1]);
+            break;
+        case 4:
+            stack.Pop();
+            if (stack.Any())
+            {
+                text = stack.Peek();
+            }
+            else
+            {
+                text = "";
+            }
+            break;
+    }
+}
