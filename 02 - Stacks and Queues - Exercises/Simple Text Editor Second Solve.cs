a986e0d29ed7b32996d7e517eafbcb9215e69e6b
Nasko-FSD <atanasnw@gmail.com>
2025-05-01 17:37:55 +0300

    Simple Text Editor Second Solve

---
+++ 02 - Stacks and Queues - Exercises/Simple Text Editor Second Solve.cs
@@ -0,0 +1,34 @@
+﻿int totalCmmands = int.Parse(Console.ReadLine());
+Stack<string> stack = new Stack<string>();
+stack.Push("");
+
+for (int i = 0; i < totalCmmands; i++)
+{
+    string[] input = Console.ReadLine()
+        .Split();
+
+    int command = int.Parse(input[0]);
+
+    switch (command)
+    {
+        case 1:
+            string toInput = input[1];
+            stack.Push(stack.Peek() + toInput);
+            break;
+
+        case 2:
+            int toErase = int.Parse(input[1]);
+            string lastInput = stack.Peek();
+            stack.Push(lastInput.Substring(0, lastInput.Length - toErase));
+            break;
+
+        case 3:
+            int atIndex = int.Parse(input[1]);
+            Console.WriteLine(stack.Peek()[atIndex - 1]);
+            break;
+
+        case 4:
+            stack.Pop();
+            break;
+    }
+}
