6c9e360f5d134fbef16ff5fd5847b5eda2f3ddae
Nasko-FSD <atanasnw@gmail.com>
2025-05-05 16:32:04 +0300

    Crossroads

---
+++ 02 - Stacks and Queues - Exercises/Crossroads.cs
@@ -0,0 +1,69 @@
+﻿int greenLight = int.Parse(Console.ReadLine());
+int freeWindow = int.Parse(Console.ReadLine());
+Queue<string> cars = new Queue<string>();
+int carsPassed = 0;
+string input;
+int collisionIndex = 0;
+bool isSuccess = true;
+string currentCar = "";
+
+while ((input = Console.ReadLine()) != "END")
+{
+    if (input == "green")
+    {
+        int timeLeft = greenLight;
+
+        while (cars.Any() && timeLeft > 0)
+        {
+            currentCar = cars.Peek();
+            int carsLength = currentCar.Length;
+
+            timeLeft -= carsLength;
+
+            if (timeLeft >= 0)
+            {
+                carsPassed++;
+                cars.Dequeue();
+            }
+            else
+            {
+                int leftToPass = Math.Abs(timeLeft);
+                if (leftToPass <= freeWindow)
+                {
+                    carsPassed++;
+                    cars.Dequeue();
+                    break;
+                }
+                else
+                {
+                    collisionIndex = carsLength - (leftToPass - freeWindow);
+                    isSuccess = false;
+                    break;
+                }
+            }
+        }
+
+        if (isSuccess == false)
+        {
+            break;
+        }
+    }
+
+    else
+    {
+        cars.Enqueue(input);
+    }    
+}
+
+
+    if (isSuccess)
+    {
+        Console.WriteLine("Everyone is safe.");
+        Console.Write($"{carsPassed} total cars passed the crossroads.");
+    }
+    else
+    {
+        char atChar = currentCar[collisionIndex];
+        Console.WriteLine("A crash happened!");
+        Console.Write($"{currentCar} was hit at {atChar}.");
+    }
