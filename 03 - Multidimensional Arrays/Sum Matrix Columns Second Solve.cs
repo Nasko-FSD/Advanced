2ad95c88d87f7e902ef361ab5a41b3ccd0c8d33c
Nasko-FSD <atanasnw@gmail.com>
2025-05-14 14:33:56 +0300

    Sum Matrix Columns Second Solve

---
+++ 03 - Multidimensional Arrays/Sum Matrix Columns Second Solve.cs
@@ -0,0 +1,30 @@
+﻿int[] matrixSize = ParseFromConsole();
+int rowsCount = matrixSize[0];
+int colsCount = matrixSize[1];
+
+int[,] currentMatrix = new int[rowsCount, colsCount];
+int[] columnSums = new int[colsCount];
+
+for (int row = 0; row < rowsCount; row++)
+{
+    int[] input = ParseFromConsole();
+
+    for (int col = 0; col < colsCount; col++)
+    {
+        currentMatrix[row, col] = input[col];
+        columnSums[col] += input[col]; 
+    }
+}
+
+foreach (var sum in columnSums)
+{
+    Console.WriteLine(sum);
+}
+
+static int[] ParseFromConsole()
+{
+    return Console.ReadLine()
+        .Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse)
+        .ToArray();
+}
