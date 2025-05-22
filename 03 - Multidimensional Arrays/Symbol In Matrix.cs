f185575559dcd3fa9fc95a235b28a204a1d40ceb
Nasko-FSD <atanasnw@gmail.com>
2025-05-22 22:25:15 +0300

    Symbol In Matrix

---
+++ 03 - Multidimensional Arrays/Symbol In Matrix.cs
@@ -0,0 +1,29 @@
+﻿int totalRows = int.Parse(Console.ReadLine());
+char[,] matrix = new char[totalRows, totalRows];
+
+for (int row = 0; row < totalRows; row++)
+{
+    string input = Console.ReadLine();
+
+    for (int col = 0; col < totalRows; col++)
+    {
+        matrix[row, col] = input[col];
+    }
+}
+
+char searchedSymbol = char.Parse(Console.ReadLine());
+
+for (int row = 0; row < totalRows; row++)
+{
+
+    for (int col = 0; col < totalRows; col++)
+    {
+        if (matrix[row, col] == searchedSymbol)
+        {
+            Console.WriteLine($"({row}, {col})");
+            Environment.Exit(0);
+        }
+    }
+}
+
+Console.WriteLine($"{searchedSymbol} does not occur in the matrix");
