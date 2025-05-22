6e31488e68d72a0ab1e71f82eae3fe3f865e542b
Nasko-FSD <atanasnw@gmail.com>
2025-05-22 21:25:33 +0300

    Primary Diagonal

---
+++ 03 - Multidimensional Arrays/Primary Diagonal.cs
@@ -0,0 +1,19 @@
+﻿int totalRows = int.Parse(Console.ReadLine());
+int[,] matrix = new int[totalRows, totalRows];
+int sumDiagonal = 0;
+for (int row = 0; row < totalRows; row++)
+{
+    int[] numbers = Console.ReadLine()
+        .Split()
+        .Select(int.Parse)
+        .ToArray();
+
+    for (int col = 0; col < totalRows; col++)
+    {
+        matrix[row, col] = numbers[col];
+    }
+
+    sumDiagonal += matrix[row, row];
+}
+
+Console.WriteLine(sumDiagonal);
