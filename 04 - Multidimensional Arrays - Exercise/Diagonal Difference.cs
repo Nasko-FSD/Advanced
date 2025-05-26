5f3087cfd146dd0eafb62443f90a922092868f41
Nasko-FSD <atanasnw@gmail.com>
2025-05-26 15:28:57 +0300

    Diagonal Difference

---
+++ 04 - Multidimensional Arrays - Exercise/Diagonal Difference.cs
@@ -0,0 +1,25 @@
+﻿int totalSize = int.Parse(Console.ReadLine());
+int[,] matrix = new int[totalSize, totalSize];
+int primaryDiagonal = 0;
+int secondaryDiagonal = 0;
+
+for (int row = 0; row < totalSize; row++)
+{
+    int[] input = Console.ReadLine()
+        .Split()
+        .Select(int.Parse)
+        .ToArray();
+
+    for (int col = 0; col < totalSize; col++)
+    {
+        matrix[row, col] = input[col];
+
+        if (col == totalSize - 1)
+        {
+            secondaryDiagonal += matrix[row, col - row];
+        }
+    }
+    primaryDiagonal += matrix[row, row];
+}
+int diffDiagonal = diffDiagonal = Math.Abs(primaryDiagonal - secondaryDiagonal);
+Console.WriteLine($"{diffDiagonal}");
