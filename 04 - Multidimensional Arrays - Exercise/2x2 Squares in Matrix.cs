0d4a3ae54dc30d2ffceb2dbbe2318d7a01e77fa1
Nasko-FSD <atanasnw@gmail.com>
2025-05-26 17:01:17 +0300

    2x2 Squares in Matrix

---
+++ 04 - Multidimensional Arrays - Exercise/2x2 Squares in Matrix.cs
@@ -0,0 +1,34 @@
+﻿int[] sizeMatrix = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+char[,] matrix = new char[sizeMatrix[0], sizeMatrix[1]];
+int match = 0;
+
+for (int row = 0; row < matrix.GetLength(0); row++)
+{
+    char[] input = Console.ReadLine()
+        .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+        .Select(char.Parse)
+        .ToArray();
+
+    for (int col = 0; col < matrix.GetLength(1); col++)
+    {
+        matrix[row, col] = input[col];
+    }
+}
+
+for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+{
+    for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+    {
+        if (matrix[row, col] == matrix[row, col + 1] &&
+            matrix[row, col] == matrix[row + 1, col] &&
+            matrix[row, col] == matrix[row + 1, col + 1])
+        {
+            match++;
+        }
+    }
+}
+Console.WriteLine(match);
