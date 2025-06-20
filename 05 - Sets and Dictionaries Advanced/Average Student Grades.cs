5f08b87b4fa941690ba4769f0eebe72a9721073c
Nasko-FSD <atanasnw@gmail.com>
2025-06-20 16:45:11 +0300

    Average Student Grades

---
+++ 05 - Sets and Dictionaries Advanced/Average Student Grades.cs
@@ -0,0 +1,28 @@
+﻿int totalStudents = int.Parse(Console.ReadLine());
+
+Dictionary<string, List<decimal>> allStudents = new Dictionary<string, List<decimal>>();
+
+for (int i = 0; i < totalStudents; i++)
+{
+    string[] input = Console.ReadLine()
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    string name = input[0];
+    decimal allGrades = decimal.Parse(input[1]);
+
+
+    if (! allStudents.ContainsKey(name))
+    {
+        allStudents.Add(name, new List<decimal>());
+    }
+    allStudents[name].Add(allGrades);
+}
+
+foreach (var student in allStudents)
+{
+    decimal totalSum = student.Value.Sum();
+    int totalGrades = student.Value.Count();
+    decimal averageGrades = totalSum / totalGrades;
+    string gradesFormatted = string.Join(" ", student.Value.Select(g => $"{g:F2}"));
+
+    Console.WriteLine($"{student.Key} -> {gradesFormatted} (avg: {averageGrades:F2})");
+}
