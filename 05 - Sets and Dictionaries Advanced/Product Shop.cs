6da0d88182aec36b7df0b9bf5817f8eaf7109d69
Nasko-FSD <atanasnw@gmail.com>
2025-06-23 16:32:34 +0300

    Product Shop

---
+++ 05 - Sets and Dictionaries Advanced/Product Shop.cs
@@ -0,0 +1,30 @@
+﻿SortedDictionary<string, Dictionary<string, double>> allShops = new SortedDictionary<string, Dictionary<string, double>>();
+
+string input = Console.ReadLine();
+
+while (input != "Revision")
+{
+    string[] inputInfo = input
+        .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+    string shopName = inputInfo[0];
+    string product = inputInfo[1];
+    double price = double.Parse(inputInfo[2]);
+
+    if (!allShops.ContainsKey(shopName))
+    {
+        allShops.Add(shopName, new Dictionary<string, double>());
+    }
+    
+    allShops[shopName][product] = price;
+
+    input = Console.ReadLine();
+}
+foreach (var shop in allShops)
+{
+    Console.WriteLine($"{shop.Key}->");
+    foreach (var products in shop.Value)
+    {
+        Console.WriteLine($"Product: {products.Key}, Price: {products.Value}");
+    }
+}
