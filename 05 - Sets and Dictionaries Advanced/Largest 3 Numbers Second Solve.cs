5f6f7e1ffde37f9b4ffe308e08287c9d41fd969c
Nasko-FSD <atanasnw@gmail.com>
2025-06-24 12:10:52 +0300

    Largest 3 Numbers Second Solve

---
+++ 05 - Sets and Dictionaries Advanced/Largest 3 Numbers Second Solve.cs
@@ -0,0 +1,16 @@
+﻿int[] inputNumbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+int[] sorted = inputNumbers.OrderByDescending(x => x).ToArray();
+int totalLength = 3;
+
+if (sorted.Length < totalLength)
+{
+    totalLength = sorted.Length;
+}
+for (int i = 0; i < totalLength; i++)
+{
+    Console.Write(sorted[i] + " ");
+}
