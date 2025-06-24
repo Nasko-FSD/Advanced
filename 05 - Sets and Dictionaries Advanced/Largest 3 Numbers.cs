66af495b0aa21cd9ffecdb391ca4a7ab0abbd43b
Nasko-FSD <atanasnw@gmail.com>
2025-06-24 12:10:21 +0300

    Largest 3 Numbers

---
+++ 05 - Sets and Dictionaries Advanced/Largest 3 Numbers.cs
@@ -0,0 +1,8 @@
+﻿int[] inputNumbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+var sorted = inputNumbers.OrderByDescending(x => x).Take(3);
+
+Console.WriteLine(string.Join(" ", sorted));
