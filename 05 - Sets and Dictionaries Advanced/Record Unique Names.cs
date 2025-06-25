180a2974706d67cdd7c18a35f80db18a64f44129
Nasko-FSD <atanasnw@gmail.com>
2025-06-25 16:37:07 +0300

    Record Unique Names

---
+++ 05 - Sets and Dictionaries Advanced/Record Unique Names.cs
@@ -0,0 +1,10 @@
+﻿int totalNames = int.Parse(Console.ReadLine());
+HashSet<string> allNames = new HashSet<string>();
+for (int i = 0; i < totalNames; i++)
+{
+    allNames.Add(Console.ReadLine());
+}
+foreach (var name in allNames)
+{
+    Console.WriteLine(name);
+}
