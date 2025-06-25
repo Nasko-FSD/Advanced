4558c4fd99efdfdbea66e921e8b4df1a410ca5c0
Nasko-FSD <atanasnw@gmail.com>
2025-06-25 16:38:24 +0300

    Parking Lot

---
+++ 05 - Sets and Dictionaries Advanced/Parking Lot.cs
@@ -0,0 +1,33 @@
+﻿HashSet<string> allCars = new HashSet<string>();
+
+string input = Console.ReadLine();
+
+while (input != "END")
+{
+    string[] inputInfo = input
+        .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+    string direction = inputInfo[0];
+    string carNumber = inputInfo[1];
+
+    if (direction == "IN")
+    {
+        allCars.Add(carNumber);
+    }
+    else
+    {
+        allCars.Remove(carNumber);
+    }
+
+    input = Console.ReadLine();
+}
+if (allCars.Count > 0)
+{
+    foreach (var number in allCars)
+    {
+        Console.WriteLine(number);
+    }
+}
+else
+{
+    Console.WriteLine("Parking Lot is Empty");
+}
