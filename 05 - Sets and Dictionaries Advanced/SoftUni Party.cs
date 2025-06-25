d7f01c1f366f973095ea0d1ffb1d56429bd35194
Nasko-FSD <atanasnw@gmail.com>
2025-06-25 19:37:31 +0300

    SoftUni Party

---
+++ 05 - Sets and Dictionaries Advanced/SoftUni Party.cs
@@ -0,0 +1,34 @@
+﻿HashSet<string> allGuests = new HashSet<string>();
+
+string input = Console.ReadLine();
+
+while (input != "PARTY")
+{
+    allGuests.Add(input);
+    input = Console.ReadLine();
+}
+
+input = Console.ReadLine();
+while (input != "END")
+{
+    allGuests.Remove(input);
+    input = Console.ReadLine();
+}
+
+List<string> vipGuests = allGuests
+    .Where(guest => char.IsDigit(guest[0]))
+    .ToList();
+
+List<string> regularGuests = allGuests
+    .Where(guest => !char.IsDigit(guest[0]))
+    .ToList();
+
+Console.WriteLine(allGuests.Count);
+foreach (var guest in vipGuests)
+{
+    Console.WriteLine(guest);
+}
+foreach (var guest in regularGuests)
+{
+    Console.WriteLine(guest);
+}
