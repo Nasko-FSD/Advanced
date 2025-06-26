dc860e6dd9aa6aa1a6058508daa10195e31c31a4
Nasko-FSD <atanasnw@gmail.com>
2025-06-26 10:10:10 +0300

    Unique Usernames

---
+++ 06 - Sets and Dictionaries Advanced - Exercises/Unique Usernames.cs
@@ -0,0 +1,13 @@
+﻿HashSet<string> allNames = new HashSet<string>();
+int totalNames = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < totalNames; i++)
+{
+    string inputName = Console.ReadLine();
+
+    allNames.Add(inputName);
+}
+foreach (string inputName in allNames)
+{
+    Console.WriteLine(inputName);
+}
