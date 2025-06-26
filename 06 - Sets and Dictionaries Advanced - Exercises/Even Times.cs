d7583d576c596952cde21c9eba892b988b044ba7
Nasko-FSD <atanasnw@gmail.com>
2025-06-26 14:08:27 +0300

    Even Times

---
+++ 06 - Sets and Dictionaries Advanced - Exercises/Even Times.cs
@@ -0,0 +1,22 @@
+﻿Dictionary<int, int> allNumbers = new Dictionary<int, int>();
+int totalInput = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < totalInput; i++)
+{
+    int currentNumber = int.Parse(Console.ReadLine());
+    if (!allNumbers.ContainsKey(currentNumber))
+    {
+        allNumbers.Add(currentNumber, 1);
+    }
+    else
+    {
+        allNumbers[currentNumber]++;
+    }
+}
+foreach (var (number, count) in allNumbers)
+{
+    if (count % 2 == 0)
+    {
+        Console.WriteLine(number);
+    }
+}
