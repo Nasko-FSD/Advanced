a5bd306be6fb628ad73477b1f5dc8c4df333d0f3
Nasko-FSD <atanasnw@gmail.com>
2025-06-26 13:15:13 +0300

    Periodic Table

---
+++ 06 - Sets and Dictionaries Advanced - Exercises/Periodic Table.cs
@@ -0,0 +1,16 @@
+﻿HashSet<string> names = new HashSet<string>();
+int totalRows = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < totalRows; i++)
+{
+    string[] input = Console.ReadLine()
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    foreach (var name in input)
+    {
+        names.Add(name);
+    }
+}
+names = names
+    .OrderBy(x => x)
+    .ToHashSet();
+Console.WriteLine(string.Join(" ", names));
