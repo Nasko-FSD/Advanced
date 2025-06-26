2ddea5be256f81c3a24b81a852a5d6ee8be92d63
Nasko-FSD <atanasnw@gmail.com>
2025-06-26 14:53:12 +0300

    Count Symbols

---
+++ 06 - Sets and Dictionaries Advanced - Exercises/Count Symbols.cs
@@ -0,0 +1,21 @@
+﻿List<char> onlyChars = Console.ReadLine()
+    .ToCharArray()
+    .ToList();
+
+Dictionary<char, int> allChars = new Dictionary<char, int>();
+
+foreach (var symbol in onlyChars)
+{
+    if (!allChars.ContainsKey(symbol))
+    {
+        allChars.Add(symbol, 1);
+    }
+    else
+    {
+        allChars[symbol]++;
+    }
+}
+foreach (var (symbol, count) in allChars.OrderBy(a => a.Key))
+{
+    Console.WriteLine($"{symbol}: {count} time/s");
+}
