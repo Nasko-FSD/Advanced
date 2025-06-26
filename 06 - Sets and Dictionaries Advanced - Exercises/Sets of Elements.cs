ba5de21d554b8e35618eb453b8814350da0e2493
Nasko-FSD <atanasnw@gmail.com>
2025-06-26 10:51:33 +0300

    Sets of Elements

---
+++ 06 - Sets and Dictionaries Advanced - Exercises/Sets of Elements.cs
@@ -0,0 +1,21 @@
+﻿HashSet<int> firstOne = new HashSet<int>();
+HashSet<int> secondOne = new HashSet<int>();
+int[] setsLength = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+for (int i = 0; i < setsLength[0]; i++)
+{
+    int inputNumbers = int.Parse(Console.ReadLine());
+    firstOne.Add(inputNumbers);
+}
+for (int i = 0; i < setsLength[1]; i++)
+{
+    int inputNumbers = int.Parse(Console.ReadLine());
+    if (firstOne.Contains(inputNumbers))
+    {
+        secondOne.Add(inputNumbers);
+    }
+}
+firstOne.RemoveWhere(x => !secondOne.Contains(x));
+Console.WriteLine(string.Join(" ", firstOne));
