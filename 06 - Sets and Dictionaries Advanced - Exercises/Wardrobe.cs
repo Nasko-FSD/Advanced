65c2400cf6fe7135b9770d39a1cd5453e3e40829
Nasko-FSD <atanasnw@gmail.com>
2025-06-27 09:01:21 +0300

    Wardrobe

---
+++ 06 - Sets and Dictionaries Advanced - Exercises/Wardrobe.cs
@@ -0,0 +1,47 @@
+﻿Dictionary<string, Dictionary<string, int>> allClothes = new Dictionary<string, Dictionary<string, int>>();
+int totalClothes = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < totalClothes; i++)
+{
+    string[] input = Console.ReadLine()
+        .Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+    string color = input[0];
+    string[] clothes = input[1].Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+    if (!allClothes.ContainsKey(color))
+    {
+        allClothes.Add(color, new Dictionary<string, int>());
+    }
+    foreach (var cloth in clothes)
+    {
+        if (!allClothes[color].ContainsKey(cloth))
+        {
+            allClothes[color].Add(cloth, 1);
+        }
+        else
+        {
+            allClothes[color][cloth]++;
+        }
+    }
+}
+string[] lookFor = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+string checkColor = lookFor[0];
+string checkCloth = lookFor[1];
+
+foreach (var (colors, clothes) in allClothes)
+{
+    Console.WriteLine($"{colors} clothes:");
+    foreach (var (cloth, amount) in clothes)
+    {
+        if (colors == checkColor &&
+            cloth == checkCloth)
+        {
+            Console.WriteLine($"* {cloth} - {amount} (found!)");
+        }
+        else
+        {
+            Console.WriteLine($"* {cloth} - {amount}");
+        }
+    }
+}
